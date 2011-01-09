a06027819821d843bcc0044a37ce5e04e5a7f60d
David Artoise Ijux <ijuxda@gmail.com>
2011-01-09 00:10:17 -0500

    Apply charlesk's piece_temp.2.patch from #532

--- libtransmission/torrent.h
+++ libtransmission/torrent.h
@@ -402,47 +402,12 @@ tr_bool tr_torrentFindFile2( const tr_torrent *, tr_file_index_t fileNo,
                              const char ** base, char ** subpath );
 
 /**
- * Get the full path of the temporary piece file for piece
- * with index @a pieceIndex.
- *
- * @return a newly allocated string containing the full filename
- *         or NULL if it does not exist.
- */
-char * tr_torrentFindPieceTemp( const tr_torrent * tor,
-                                tr_piece_index_t   pieceIndex );
-
-/**
  * @brief Like tr_torrentFindFile2() but for temporary piece files.
  */
 tr_bool tr_torrentFindPieceTemp2( const tr_torrent  * tor,
                                   tr_piece_index_t    pieceIndex,
                                   const char       ** base,
                                   char             ** subpath );
-
-/**
- * @brief Get the directory where temporary piece files are stored.
- */
-const char * tr_torrentGetPieceTempDir( const tr_torrent * tor );
-
-/**
- * @brief Delete all temporary piece files for the torrent.
- */
-void tr_torrentRemovePieceTemp( tr_torrent * tor );
-
-/**
- * All data in temporary pieces files is removed from the torrent.
- *
- * @note No validation or locking is done on @a tor.
- */
-void tr_torrentInvalidatePieceTemp( tr_torrent * tor );
-
-/**
- * Remove all temporary piece files used by file with index @a fileIndex.
- *
- * @note No validation or locking is done on the arguments.
- */
-void tr_torrentInvalidatePieceTempFile( tr_torrent      * tor,
-                                        tr_file_index_t   fileIndex );
 
 /* Returns a newly-allocated version of the tr_file.name string
  * that's been modified to denote that it's not a complete file yet.
