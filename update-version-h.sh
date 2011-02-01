c6ec7eb734289dd2b9dd784a517e3289c58d0703
David Artoise Ijux <ijuxda@gmail.com>
2011-02-01 15:33:19 -0500

    Add svn revision to version string if it exists.

--- update-version-h.sh
+++ update-version-h.sh
@@ -28,9 +28,13 @@ if [ -d ".svn" ] && type svnversion >/dev/null 2>&1; then
 elif head=`git rev-parse --verify --short HEAD 2>/dev/null`; then
     svn_revision=`git log --grep=git-svn-id -n1 --pretty=format:"%b" | tail -n1 | awk '{print $2}' | cut -d@ -f2`
     if test "x$svn_revision" = "x" ; then
-      svn_revision=0
+        svn_revision=0
     fi
-    source_version="git:${head}"
+    if test $svn_revision -eq 0 ; then
+        source_version="git:$head"
+    else
+        source_version="git:$head svn:$svn_revision"
+    fi
 else
     # Give up and check the source files
     svn_revision=`awk '/\\$Id: /{ if ($4>i) i=$4 } END {print i}' */*.cc */*.[chm] */*.po`
