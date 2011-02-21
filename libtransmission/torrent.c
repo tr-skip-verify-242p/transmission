b334d8d1892adfa496b0c6323ad24cf09b3ef5db
David Artoise Ijux <ijuxda@gmail.com>
2011-02-21 02:34:30 -0500

    Empty torrents do not wait for verification.

--- libtransmission/torrent.c
+++ libtransmission/torrent.c
@@ -790,6 +790,22 @@ setLocalErrorIfFilesDisappeared( tr_torrent * tor )
     return disappeared;
 }
 
+static tr_bool fileExists( const char * );
+
+static tr_bool
+torrentDataExists( const tr_torrent * tor )
+{
+    char * path;
+    tr_bool exists;
+    if( !tr_torrentHasMetadata( tor ) )
+        return FALSE;
+    path = tr_buildPath( tr_torrentGetCurrentDir( tor ),
+                         tr_torrentName( tor ), NULL );
+    exists = fileExists( path );
+    tr_free( path );
+    return exists;
+}
+
 static void
 torrentInit( tr_torrent * tor, const tr_ctor * ctor )
 {
@@ -1685,6 +1701,17 @@ torrentRecheckDoneCB( tr_torrent * tor )
     tr_runInEventThread( tor->session, torrentRecheckDoneImpl, tor );
 }
 
+static tr_bool
+quickVerify( tr_torrent * tor )
+{
+    if( torrentDataExists( tor ) )
+        return FALSE;
+    tr_cpReset( &tor->completion );
+    tor->anyDate = tr_time( );
+    tr_torrentSetDirty( tor );
+    return TRUE;
+}
+
 static void
 verifyTorrent( void * vtor )
 {
@@ -1706,7 +1733,12 @@ verifyTorrent( void * vtor )
     if( setLocalErrorIfFilesDisappeared( tor ) )
         tor->startAfterVerify = FALSE;
     else
-        tr_verifyAdd( tor, torrentRecheckDoneCB );
+    {
+        if( quickVerify( tor ) )
+            torrentRecheckDoneCB( tor );
+        else
+            tr_verifyAdd( tor, torrentRecheckDoneCB );
+    }
 
     tr_sessionUnlock( tor->session );
 }
