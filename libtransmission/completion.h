22719e1193eeab83034bf543cb7c860bf8a0a623
David Artoise Ijux <ijuxda@gmail.com>
2011-02-24 19:02:53 -0500

    Remove unneeded prototype.

--- libtransmission/completion.h
+++ libtransmission/completion.h
@@ -130,9 +130,6 @@ int tr_cpMissingBlocksInPiece( const tr_completion * cp, tr_piece_index_t i );
 uint64_t tr_cpMissingBytesInPiece( const tr_completion * cp,
                                    tr_piece_index_t      piece );
 
-tr_bool  tr_cpPieceIsComplete( const tr_completion * cp,
-                               tr_piece_index_t      piece );
-
 static inline tr_bool
 tr_cpPieceIsComplete( const tr_completion * cp, tr_piece_index_t i )
 {
