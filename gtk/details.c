ac27478ee22da91c507027fff5a829c57a39e28f
David Artoise Ijux <ijuxda@gmail.com>
2011-03-08 21:30:39 -0500

    Show tracker protocol in gtk tracker details.

--- gtk/details.c
+++ gtk/details.c
@@ -1805,14 +1805,20 @@ buildTrackerSummary( const char * key, const tr_tracker_stat * st, gboolean show
     /* hostname */
     {
         const char * host = st->host;
-        const char * pch = strstr( host, "://" );
-        if( pch )
-            host = pch + 3;
+        char proto[16], * pch;
+        tr_strlcpy( proto, st->announce, sizeof( proto ) );
+        if( ( pch = strstr( proto, "://" ) ) )
+        {
+            pch[0] = ' ';
+            pch[1] = '\0';
+        }
+        else
+            proto[0] = '\0';
         g_string_append( gstr, st->isBackup ? "<i>" : "<b>" );
         if( key )
-            str = g_markup_printf_escaped( "%s - %s", host, key );
+            str = g_markup_printf_escaped( "%s%s - %s", proto, host, key );
         else
-            str = g_markup_printf_escaped( "%s", host );
+            str = g_markup_printf_escaped( "%s%s", proto, host );
         g_string_append( gstr, str );
         g_free( str );
         g_string_append( gstr, st->isBackup ? "</i>" : "</b>" );
